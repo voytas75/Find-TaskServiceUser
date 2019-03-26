7289b439d1d0fc92e8758bec2aa3c602bff05850
Wojciech Napierała <wnapierala@hotmail.com>
2019-03-26 19:59:08 +0100

    New examples

--- Find-TaskServiceUser.ps1
+++ Find-TaskServiceUser.ps1
@@ -102,8 +102,10 @@
     if (!$service -and !$task) {
     Write-Output '
     Examples:
-      Find-TaskServiceUser -Computer computer_name -User User -Service -Task
+      Find-TaskServiceUser -Computer computerA -User UserA -Service -Task
+      Find-TaskServiceUser -Computer computerB -User UserB -Task -Log 
       "comp1","comp2" | Find-TaskServiceUser -Service -Task
+      "comp3" | Find-TaskServiceUser -Service
     '
     }
     function LogWrite {
