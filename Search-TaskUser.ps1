e6fe9b8bcb6e20f3b65d65ad02988aa1fcdacda9
Wojciech Napierała <wnapierala@hotmail.com>
2019-03-28 21:28:24 +0100

    add condition on localhost

--- Search-TaskUser.ps1
+++ Search-TaskUser.ps1
@@ -7,12 +7,21 @@ Function Search-TaskUser {
     )
     process {
         Write-Verbose -Message 'running system command ''schtasks'''
-        try {
-            $task_=Invoke-Expression "schtasks /query /s $server /fo csv /v" -ErrorAction Stop
-        }
-        catch {
-            Write-Error -Message "Failed to invoke ""schtasks"": $_"
-        }
+        if ($server -match $env:COMPUTERNAME) {
+            try {
+                $task_=Invoke-Expression "schtasks /query /fo csv /v" -ErrorAction Stop
+            }
+            catch {
+                Write-Error -Message "Failed to invoke ""schtasks"": $_"
+            }
+        } else {
+            try {
+                $task_=Invoke-Expression "schtasks /query /s $server /fo csv /v" -ErrorAction Stop
+            }
+            catch {
+                Write-Error -Message "Failed to invoke ""schtasks"": $_"
+            }
+        } 
         $match_ = "$user"
         Write-Verbose -Message 'filter tasks'
         $task_ | Where-Object {$_ -match $match_} 
