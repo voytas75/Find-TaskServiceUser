ac0185039c37c344ac1759d83aeffad923d2bee1
Wojciech Napierała <wnapierala@hotmail.com>
2019-04-18 23:25:59 +0200

    #62

--- Find-ServiceUser.ps1
+++ Find-ServiceUser.ps1
@@ -15,28 +15,30 @@ Function Find-ServiceUser {
     )
     $user = $user.trim()
     $computer = $computer.trim()
-    if (Test-Connection -ComputerName $computer -Count 1 -Quiet -ErrorAction SilentlyContinue) {
-        if ($Strict) {
-            $filter = "startname = '$($user)'"
-            #Write-Information $filter -InformationAction Continue
-        } else {
-            $filter = "startname LIKE '%$($user)%'"
-        }
-        Write-Verbose -Message "WMI query for system services."
-        try {
-            $service_ = Get-CimInstance -classname win32_service -filter "$filter" -ComputerName $computer -ErrorAction Stop
-        } 
-        catch {
-            Write-Error -Message "Failed WMI query for system services with Service Logon Account as ""$user"": $_"
-        }
-        if ($service_) {
-            Write-Verbose -Message "Return WMI query data"
-            return $service_
-        } 
+    try {
+        Test-Connection -ComputerName $computer -Count 1 -Quiet -ErrorAction SilentlyContinue
     }
-    else {
+    catch {
         Write-Verbose -Message "$computer offline?"
         Write-Information -MessageData "$computer offline?" -InformationAction Continue
         return $null
     }
+    if ($Strict) {
+        $filter = "startname = '$($user)'"
+        #Write-Information $filter -InformationAction Continue
+    }
+    else {
+        $filter = "startname LIKE '%$($user)%'"
+    }
+    Write-Verbose -Message "WMI query for system services."
+    try {
+        $service_ = Get-CimInstance -classname win32_service -filter "$filter" -ComputerName $computer -ErrorAction Stop
+    } 
+    catch {
+        Write-Error -Message "Failed WMI query for system services with Service Logon Account as ""$user"": $_"
+    }
+    if ($service_) {
+        Write-Verbose -Message "Return WMI query data"
+        return $service_
+    } 
 }# end function Find-ServiceUser
