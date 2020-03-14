7633439908b0884da2cff5e5717bfe5da661bc83
Wojciech Napierała <wnapierala@hotmail.com>
2020-03-14 12:47:04 +0100

    comment unused variable

--- Find-TaskServiceUser.ps1
+++ Find-TaskServiceUser.ps1
@@ -279,7 +279,7 @@ DONATION: If you want to support my work https://www.paypal.com/cgi-bin/webscr?c
             Write-Information -MessageData "Export XML File: $($Exportpath)" -InformationAction Continue
             Write-Information -MessageData "Export XML File: You can import file using 'Import-Clixml `"$($Exportpath)`"'" -InformationAction Continue
             $task_all_unique = $tasks_all | Sort-Object taskname -Unique
-            $services_all_unique = $services_all | Sort-Object name -Unique
+            #$services_all_unique = $services_all | Sort-Object name -Unique
             $export_data = @{"Tasks" = $task_all_unique; "Services" = $services_all }
             #Add-Content -LiteralPath $Exportpath -Value $export_data -PassThru
             Export-Clixml -LiteralPath $Exportpath -InputObject $export_data
@@ -288,7 +288,7 @@ DONATION: If you want to support my work https://www.paypal.com/cgi-bin/webscr?c
             Write-Information -MessageData "Export JSON File: $($Exportjsonpath)" -InformationAction Continue
             Write-Information -MessageData "Export JSON File: You can import file using '`$json_data = Get-Content -Raw -Path `"$($Exportjsonpath)`" | ConvertFrom-Json'" -InformationAction Continue
             $task_all_unique = $tasks_all | Sort-Object taskname -Unique
-            $services_all_unique = $services_all | Sort-Object name -Unique
+            #$services_all_unique = $services_all | Sort-Object name -Unique
             $export_data = @{"Tasks" = $task_all_unique; "Services" = $services_all }
             $export_data | ConvertTo-Json | out-file $Exportjsonpath
         }
