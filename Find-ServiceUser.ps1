4c3fdf2900cdc62d16d6f8f74a4fe6885b293531
Wojciech Napierała <wnapierala@udt.gov.pl>
2020-03-29 19:04:30 +0200

    minor changes ; add verbose ;syntax change

--- Find-ServiceUser.ps1
+++ Find-ServiceUser.ps1
@@ -18,11 +18,12 @@ Function Find-ServiceUser {
     $service_ = $false
 
     try {
+        Write-Verbose -Message "Test connection to computer $computer"
         Test-Connection -ComputerName $computer -Count 1 -Quiet -ErrorAction SilentlyContinue | Out-Null
     }
     catch {
-        Write-Verbose -Message "$computer offline?"
-        Write-Information -MessageData "$computer offline?" -InformationAction Continue
+        Write-Verbose -Message "Error testing connection to computer $($computer.toupper()). Offline?"
+        Write-Information -MessageData "Error testing connection to computer $($computer.toupper()). Offline?" -InformationAction Continue
         return $null
     }
     if ($Strict) {
@@ -34,20 +35,20 @@ Function Find-ServiceUser {
     }
     Write-Verbose -Message "WMI query for system services."
     #if computer is localhost then WMI query has no -computername
-    if ($computer -eq $env:COMPUTERNAME) {
+    if ($computer -eq $env:COMPUTERNAME -or $computer -eq "localhost") {
         try {
             $service_ = Get-CimInstance -classname win32_service -filter "$filter" -ErrorAction Stop
         } 
         catch {
-            Write-Error -Message "Failed WMI query for system services with Service Logon Account as ""$user"": $_"
+            Write-Error -Message "Failed local WMI query for system services with Service Logon Account as ""$user"": $_"
         }
         #Write-Debug ((get-variable "service_").value)
         #(get-variable "service_").value
         if ($service_) {
-            Write-Verbose -Message "Return WMI query data"
+            Write-Verbose -Message "Return local WMI query data."
             return $service_
         } else {
-            Write-Verbose -Message "NO WMI query data"
+            Write-Verbose -Message "NO local WMI query data."
             $out_variable = (Get-Variable service_).Value
             Write-Debug -message "Return data from inside 'Find-ServiceUser': $out_variable" -InformationAction Continue
             return $false
