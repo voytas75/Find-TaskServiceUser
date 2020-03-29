1053f9b1f7376e5a973a7b50217ddb0e6f8f0df0
Wojciech Napierała <wnapierala@udt.gov.pl>
2020-03-29 11:02:09 +0200

    fix for #69

--- Find-TaskServiceUser.ps1
+++ Find-TaskServiceUser.ps1
@@ -113,6 +113,9 @@ DONATION: If you want to support my work https://www.paypal.com/cgi-bin/webscr?c
     )
     Begin {
         $computer_local = $false
+        if ($computer -eq $env:COMPUTERNAME) {
+            $computer_local = $true
+        }
         if ($OpenProjectSite) {
             Start-Process 'https://github.com/voytas75/Find-TaskServiceUser'
             return
@@ -124,9 +127,8 @@ DONATION: If you want to support my work https://www.paypal.com/cgi-bin/webscr?c
             if ($user -eq "Administrator") {
                 Write-Output "Set default user: ""ADMINISTRATOR"""
             }
-            if ($computer -eq $env:COMPUTERNAME) {
+            if ($computer_local) {
                 Write-Output "Set default computer: ""$($env:COMPUTERNAME.toupper())"" (localhost)"
-                $computer_local = $true
             }  
         }
         else {
